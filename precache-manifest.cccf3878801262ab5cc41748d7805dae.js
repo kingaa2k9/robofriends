5fac307ff68e192aaadb4b4fed31ac6f6e07122e
Dipin Rajendran <kingaa2k9@gmail.com>
2019-10-23 15:55:31 +0530

    Updates

---
+++ precache-manifest.cccf3878801262ab5cc41748d7805dae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b13f95c95de41e7ccb8637ca52810ae8",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "e83b72c0e4902c66e98f",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "908dd3738fbfff6a5801",
+    "url": "/robofriends/static/css/main.5f6b0ed3.chunk.css"
+  },
+  {
+    "revision": "e83b72c0e4902c66e98f",
+    "url": "/robofriends/static/js/2.5f96ed3a.chunk.js"
+  },
+  {
+    "revision": "908dd3738fbfff6a5801",
+    "url": "/robofriends/static/js/main.8daee7fe.chunk.js"
+  },
+  {
+    "revision": "69ae533dd49bc2dbc7cd",
+    "url": "/robofriends/static/js/runtime-main.9f59819d.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
